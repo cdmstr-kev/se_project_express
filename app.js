e5e2104b106f65bd0538cc677fc2afcb3d829e04
Kevin <kvnnedd@gmail.com>
2025-09-17 21:04:44 -0400

    initial commit:
    -create folder structure
    -create server entrypoint
    -configure eslint
    -test server entry point

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const { PORT = 3001 } = process.env;
+
+app.get('/', (req, res) => {
+  res.send('Hello Worlds!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
