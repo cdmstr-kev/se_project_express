bb5efd4d87022bee97f5937eeec4ed51da7a1282
Kevin <kvnnedd@gmail.com>
2025-09-17 21:37:10 -0400

    Create schema for user and clothingItem

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require("mongoose");
+
 const app = express();
+
 const { PORT = 3001 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.get('/', (req, res) => {
   res.send('Hello Worlds!');
