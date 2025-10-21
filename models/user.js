ec3ae7b7718c11d1b263ddfdf82fd6ca670be30d
Kevin <kvnnedd@gmail.com>
2025-10-21 19:33:32 -0400

    feat: update user model to include email and password

--- models/user.js
+++ models/user.js
@@ -15,8 +15,25 @@ const userSchema = new mongoose.Schema({
       validator(value) {
         return validator.isURL(value);
       },
-      message: "You must enter a valid URL",
+      message: (props) => `${props.value} You must enter a valid URL`,
     },
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator(email) {
+        return validator.isEmail(email);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
