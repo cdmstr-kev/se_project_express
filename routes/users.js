64554ed7d29f9770faf92907340214612df78ad8
Kevin <kvnnedd@gmail.com>
2025-10-22 17:25:51 -0400

    Clean up: Clean up the users route to remove comments

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,9 @@ const express = require("express");
 const auth = require("../middlewares/auth");
 
 const router = express.Router();
-// TODO: remove this before submitting project
 const { getCurrentUser, updateUser } = require("../controllers/users");
 
-// TODO: remove the routes below and possible delete this file
-
-// router.get("/", getAllUsers);
 router.get("/me", auth, getCurrentUser);
-// TODO change the controller in this router.
 router.patch("/me", auth, updateUser);
-// router.post("/", createUser);
 
 module.exports = router;
