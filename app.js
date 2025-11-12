b02663dae1a862dd633d3801c51f3d74bda15336
Kevin <kvnnedd@gmail.com>
2025-11-12 18:28:20 -0500

    Install winston and express winston

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
+const { errors } = require("celebrate");
 const mainRouter = require("./routes/index");
 const { NOT_FOUND } = require("./utils/errors");
 const errorHandler = require("./middlewares/error-handler");
-const { errors } = require("celebrate");
 
 const app = express();
 const { PORT = 3001 } = process.env;
