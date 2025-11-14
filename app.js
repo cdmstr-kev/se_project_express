6e8288161589311af22fc20e1f6002fa218ffca8
Kevin <kvnnedd@gmail.com>
2025-11-13 19:54:08 -0500

    Refactor: refactor the mongoose connection and app to listen for request after mongoose successfully connect

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/wtwr_db")
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ DB error:", err));
-
 app.use(requestLogger);
 
 app.use("/", mainRouter);
@@ -32,6 +27,15 @@ app.use(errorLogger);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server listening at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ DB error:", err);
+    process.exit(1);
+  });
